95bd779c4ec80db8fac1a9461c95ccfdefbc3a05
Aleksandr Honcharov <charleksvik@gmail.com>
2014-12-29 14:13:01 +0200

    + каринка, лого, телы, слоган шапки

--- allinclusive/template-header-basic.php
+++ allinclusive/template-header-basic.php
@@ -1,8 +1,19 @@
     <header class="header_basic">
-        <div class="container">
+        <div class="container">                        
             <div class="row">
-                <div class="col-xs-12">
+                <div class="col-xs-12 header_basic_wrap">
                     <img class="img-responsive" src="<?php bloginfo('template_url'); ?>/img/header-basic.jpg" alt="">
+                    <div class="header_basic_wrap__logo">
+                        All inclusive<br>
+                        <span class="header_basic_wrap__logo___text">студия интерьерного дизайна</span>
+                    </div>
+                    <div class="header_basic_wrap__phones">
+                        <p>096-707-59-70</p>
+                        <p>093-573-76-66</p>
+                    </div>
+                    <div class="header_basic_wrap__slogan">
+                        если Вы хотите, чтобы все было идеально
+                    </div>
                 </div>
             </div>
         </div>
