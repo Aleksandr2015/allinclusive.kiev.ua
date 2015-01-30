2223c013129833ec8100307254eb8f708d9def59
Aleksandr Honcharov <charleksvik@gmail.com>
2015-01-30 19:57:14 +0200

    + механика скриптов слайдера в табах на главной
    Подготовлена механика скриптов для создания слайдера картинок на главной
    старнице в табах по примеру сайта http://www.merkamueble.com/es/
    (http://joxi.ru/DrlNGJ7fnjYj2P)

--- allinclusive/header.php
+++ allinclusive/header.php
@@ -15,10 +15,10 @@
     <!-- font-awesome icons -->
     <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/vendor/font-awesome-4.2.0/css/font-awesome.css">
     <!-- latest jquery version -->
-    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
     <!-- bootstrap js -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>    
-    <!-- slider -->    
+    <!-- header slider -->    
     <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/vendor/unslider/unslider.css">
     <script src="//unslider.com/unslider.min.js"></script>
     <script>
@@ -31,7 +31,97 @@
                     fluid: true,              //  Support responsive design. May break non-responsive designs
                 });
             });        
+    </script>    
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/vendor/coolcarousel/coolcarousel.css">
+    <script src="<?php bloginfo('template_url'); ?>/vendor/coolcarousel/jquery.carouFredSel-6.2.1-packed.js"></script>
+    <script>
+      $(document).ready(function(){          
+          CustomHandler();
+      });
+      var CustomHandler = function() {
+        
+        $('.carousel ul').carouFredSel({
+          responsive: true,
+          circular: true,
+          infinite: true,
+          auto: false,
+          cookie: false,
+          prev: {
+            button: ".carousel  .prev"
+          },
+          next: {
+              button: ".carousel .next"
+          },
+          pagination: ".carousel .pagination",
+          items: {
+            visible: 4
+          },    
+          scroll: {
+              items: 1
+          }        
+				});
+        
+        $('.carousel2 ul').carouFredSel({
+           responsive: true,
+           circular: true,
+           infinite: true,
+           auto: false,
+           cookie: false,
+           prev: {
+              button: " .carousel2  .prev"
+            },
+            next: {
+                button: ".carousel2 .next"
+            },
+            pagination: ".carousel2 .pagination",
+            items: {
+              visible: 4
+            },    
+            scroll: {
+                items: 1
+            }   
+          });   
+        
+          $('.carousel3 ul').carouFredSel({
+             responsive: true,
+             circular: true,
+             infinite: true,
+             auto: false,
+             cookie: false,
+             prev: {
+                button: ".carousel3  .prev"
+              },
+              next: {
+                  button: ".carousel3 .next"
+              },
+              pagination: ".carousel3 .pagination",
+              items: {
+                visible: 4
+              },    
+              scroll: {
+                  items: 1
+              }            
+            }); 
+      }
     </script>
+    
+    <script type="text/javascript">
+        $(function() {
+              $('#tabs').carouFredSel({
+                      circular: false,
+                      items: 1,
+                      width: '100%',
+                      responsive: 'true',
+                      auto: false,
+                      pagination: {
+                        container: '#pager',
+                        anchorBuilder: false,                      
+                      },
+                scroll : { fx : "crossfade" },
+        });
+      });
+    </script>
+    
     <!--[if IE]>
         <script src="https://cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
